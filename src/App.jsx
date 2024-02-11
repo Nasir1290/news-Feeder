15ad64eb905ca664c13e1d198f5758dfedcbd33e
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-11 09:11:11 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className=' text-3xl bg-red-600'> hell </h1>
+      <h1 className=' text-3xl bg-red-600'> hello </h1>
     </div>
   )
 }
